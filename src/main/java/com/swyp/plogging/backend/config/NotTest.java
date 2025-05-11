4fed34fa412e137b571d4e9fdd0f4ab8428a0431
geniushee <129508219+geniushee@users.noreply.github.com>
2025-05-11 21:17:45 +0900

    리전서비스 지역데이터 초기화 메서드 실행 시점 수정 (#13)
    * fix: 로그 확인을 위하 코드 추가2
    
    * fix: region데이터 초기화 실행시점 변경
    
    * fix: 로그확인을 위하 코드 원복(삭제)

---
+++ src/main/java/com/swyp/plogging/backend/config/NotTest.java
@@ -0,0 +1,22 @@
+package com.swyp.plogging.backend.config;
+
+import com.swyp.plogging.backend.service.RegionService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Profile("!test")
+@RequiredArgsConstructor
+@Configuration
+public class NotTest {
+    private final RegionService regionService;
+
+    @Bean
+    public ApplicationRunner work(){
+        return args -> {
+            regionService.initRegionData();
+        };
+    }
+}
