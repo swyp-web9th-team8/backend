db953bde3593adf95735259a6ea35ced49f799b7
sspark <sauce0127@gmail.com>
2025-05-12 20:50:09 +0900

    fix : 공지사항 작성 시 body 사용 으로 변경

--- src/main/java/com/swyp/plogging/backend/announce/controller/AnnounceController.java
+++ src/main/java/com/swyp/plogging/backend/announce/controller/AnnounceController.java
@@ -1,5 +1,6 @@
 package com.swyp.plogging.backend.announce.controller;
 
+import com.swyp.plogging.backend.announce.controller.dto.AnnounceRequest;
 import com.swyp.plogging.backend.announce.controller.dto.AnnounceResponse;
 import com.swyp.plogging.backend.announce.service.AnnounceService;
 import com.swyp.plogging.backend.common.dto.ApiResponse;
@@ -9,8 +10,8 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -33,8 +34,8 @@ public class AnnounceController {
     }
 
     @PostMapping
-    public ApiResponse<AnnounceResponse> createAnnounce(@RequestParam String title, @RequestParam String content) {
-        AnnounceResponse announce = announceService.createAnnounce(title, content);
+    public ApiResponse<AnnounceResponse> createAnnounce(@RequestBody AnnounceRequest request) {
+        AnnounceResponse announce = announceService.createAnnounce(request.getTitle(), request.getContent());
         return ApiResponse.ok(announce, "Announce created successfully!");
     }
 
---
+++ src/main/java/com/swyp/plogging/backend/announce/controller/dto/AnnounceRequest.java
@@ -0,0 +1,10 @@
+package com.swyp.plogging.backend.announce.controller.dto;
+
+import lombok.Data;
+
+@Data
+public class AnnounceRequest {
+
+    private String title;
+    private String content;
+}
