386f6732b3ef1ac61e07be6cdae4dc7ccedb6a05
sspark <sauce0127@gmail.com>
2025-05-13 15:16:25 +0900

    fix : 이미지 확장자 타입 추가

--- src/main/java/com/swyp/plogging/backend/common/domain/ImageExtension.java
+++ src/main/java/com/swyp/plogging/backend/common/domain/ImageExtension.java
@@ -3,7 +3,7 @@ package com.swyp.plogging.backend.common.domain;
 import java.util.Arrays;
 
 public enum ImageExtension {
-    JPG, JPEG, PNG;
+    JPG, JPEG, PNG, TIFF;
 
     public static boolean isAllowed(String extension) {
         return Arrays.stream(values())
