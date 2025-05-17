a34d687505bd96c6afbb32e173af91e55f4e693b
sspark <sauce0127@gmail.com>
2025-05-17 19:02:15 +0900

    fix : 모임 상세 조회 응답 객체 내 참여여부 필드 추가

--- src/main/java/com/swyp/plogging/backend/post/sevice/PostService.java
+++ src/main/java/com/swyp/plogging/backend/post/sevice/PostService.java
@@ -13,6 +13,13 @@ import com.swyp.plogging.backend.post.repository.PostRepository;
 import com.swyp.plogging.backend.service.RegionService;
 import com.swyp.plogging.backend.user.domain.AppUser;
 import jakarta.annotation.Nullable;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -23,14 +30,6 @@ import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.time.LocalDateTime;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-import java.util.stream.Collectors;
 
 @Slf4j
 @Service
@@ -47,10 +46,10 @@ public class PostService {
      */
     @Transactional
     public PostDetailResponse createPost(AppUser user, String title, String content,
-                                         LocalDateTime meetingTime, String placeId,
-                                         String placeName, String address,
-                                         @NonNull Integer maxParticipants, String openChatUrl,
-                                         @Nullable Integer deadLine) {
+        LocalDateTime meetingTime, String placeId,
+        String placeName, String address,
+        @NonNull Integer maxParticipants, String openChatUrl,
+        @Nullable Integer deadLine) {
 
         if (maxParticipants <= 0) {
             throw new IllegalArgumentException("최대인원 설정이 잘못되었습니다.");
@@ -76,24 +75,24 @@ public class PostService {
         // 네이버 지도에서 도로명 주소로 위치를 검색 후 위도경도 입력
         List<Map<String, Object>> list = locationService.searchCoordinatesByAddress(address);
         Map<String, Object> location = list.stream().filter(
-                        map -> {
-                            Matcher responseMatcher1 = pattern1.matcher((CharSequence) map.get("roadAddress"));
-                            Matcher responseMatcher2 = pattern2.matcher((CharSequence) map.get("roadAddress"));
-                            if (r1 != null && responseMatcher1.find()) {
-                                // 구, 길, 번호
-                                return responseMatcher1.group(1).equals(r1) &&
-                                        responseMatcher1.group(2).equals(r2) &&
-                                        responseMatcher1.group(3).equals(r3);
+                map -> {
+                    Matcher responseMatcher1 = pattern1.matcher((CharSequence) map.get("roadAddress"));
+                    Matcher responseMatcher2 = pattern2.matcher((CharSequence) map.get("roadAddress"));
+                    if (r1 != null && responseMatcher1.find()) {
+                        // 구, 길, 번호
+                        return responseMatcher1.group(1).equals(r1) &&
+                            responseMatcher1.group(2).equals(r2) &&
+                            responseMatcher1.group(3).equals(r3);
 
-                            }else if(responseMatcher2.find()){
-                                // 길, 번호
-                                return responseMatcher2.group(1).equals(r2) &&
-                                        responseMatcher2.group(2).equals(r3);
-                            }
-                            return false;
-                        })
-                .findFirst()
-                .orElseThrow(() -> new RuntimeException("찾는 주소가 없습니다."));
+                    } else if (responseMatcher2.find()) {
+                        // 길, 번호
+                        return responseMatcher2.group(1).equals(r2) &&
+                            responseMatcher2.group(2).equals(r3);
+                    }
+                    return false;
+                })
+            .findFirst()
+            .orElseThrow(() -> new RuntimeException("찾는 주소가 없습니다."));
         Double latitude = (Double) location.get("latitude");
         Double longitude = (Double) location.get("longitude");
 
@@ -103,20 +102,20 @@ public class PostService {
         }
 
         Post post = Post.builder()
-                .writer(user)
-                .title(title)
-                .content(content)
-                .meetingDt(meetingTime)
-                .placeId(placeId)
-                .placeName(placeName)
-                .address(address)
-                .completed(false)
-                .maxParticipants(maxParticipants)
-                .openChatUrl(openChatUrl)
-                .latitude(latitude)
-                .longitude(longitude)
-                .location(point)
-                .build();
+            .writer(user)
+            .title(title)
+            .content(content)
+            .meetingDt(meetingTime)
+            .placeId(placeId)
+            .placeName(placeName)
+            .address(address)
+            .completed(false)
+            .maxParticipants(maxParticipants)
+            .openChatUrl(openChatUrl)
+            .latitude(latitude)
+            .longitude(longitude)
+            .location(point)
+            .build();
 
         // null일 경우 30분전 세팅
         post.setUpDeadLine(deadLine);
@@ -130,11 +129,11 @@ public class PostService {
      */
     @Transactional
     public PostDetailResponse createPost(AppUser user, String title, String content,
-                                         LocalDateTime meetingTime, String placeId,
-                                         String placeName, String address,
-                                         Double latitude, Double longitude,
-                                         @NonNull Integer maxParticipants, String openChatUrl,
-                                         @Nullable Integer deadLine) {
+        LocalDateTime meetingTime, String placeId,
+        String placeName, String address,
+        Double latitude, Double longitude,
+        @NonNull Integer maxParticipants, String openChatUrl,
+        @Nullable Integer deadLine) {
 
         if (maxParticipants <= 0) {
             throw new IllegalArgumentException("최대인원 설정이 잘못되었습니다.");
@@ -150,20 +149,20 @@ public class PostService {
         Point location = locationService.createPoint(longitude, latitude);
 
         Post post = Post.builder()
-                .writer(user)
-                .title(title)
-                .content(content)
-                .meetingDt(meetingTime)
-                .placeId(placeId)
-                .placeName(placeName)
-                .address(address)
-                .latitude(latitude)
-                .longitude(longitude)
-                .location(location)
-                .completed(false)
-                .maxParticipants(maxParticipants)
-                .openChatUrl(openChatUrl)
-                .build();
+            .writer(user)
+            .title(title)
+            .content(content)
+            .meetingDt(meetingTime)
+            .placeId(placeId)
+            .placeName(placeName)
+            .address(address)
+            .latitude(latitude)
+            .longitude(longitude)
+            .location(location)
+            .completed(false)
+            .maxParticipants(maxParticipants)
+            .openChatUrl(openChatUrl)
+            .build();
 
         // null일 경우 30분전 세팅
         post.setUpDeadLine(deadLine);
@@ -178,18 +177,18 @@ public class PostService {
     @Transactional
     public PostDetailResponse createPost(CreatePostRequest request, AppUser user) {
         return createPost(
-                user,
-                request.getTitle(),
-                request.getContent(),
-                request.getMeetingTime(),
-                request.getPlaceId(),
-                request.getPlaceName(),
-                request.getAddress(),
-                request.getLatitude(),
-                request.getLongitude(),
-                request.getMaxParticipants(),
-                request.getOpenChatUrl(),
-                30 // 기본값으로 30분 전 설정
+            user,
+            request.getTitle(),
+            request.getContent(),
+            request.getMeetingTime(),
+            request.getPlaceId(),
+            request.getPlaceName(),
+            request.getAddress(),
+            request.getLatitude(),
+            request.getLongitude(),
+            request.getMaxParticipants(),
+            request.getOpenChatUrl(),
+            30 // 기본값으로 30분 전 설정
         );
     }
 
@@ -198,9 +197,9 @@ public class PostService {
      */
     @Transactional
     public PostDetailResponse modifyPost(AppUser user, Long id, String title,
-                                         String content, LocalDateTime meetingTime,
-                                         String placeId, String placeName, String address,
-                                         Integer maxParticipants, String openChatUrl, Integer deadLine) {
+        String content, LocalDateTime meetingTime,
+        String placeId, String placeName, String address,
+        Integer maxParticipants, String openChatUrl, Integer deadLine) {
 
         Post post = findById(id);
 
@@ -210,10 +209,10 @@ public class PostService {
         }
 
         post.modify(
-                title, content, meetingTime,
-                placeId, placeName, address,
-                null, null, // 위치 정보는 변경하지 않음
-                maxParticipants, openChatUrl, deadLine
+            title, content, meetingTime,
+            placeId, placeName, address,
+            null, null, // 위치 정보는 변경하지 않음
+            maxParticipants, openChatUrl, deadLine
         );
 
         return post.toDetailResponse();
@@ -224,10 +223,10 @@ public class PostService {
      */
     @Transactional
     public PostDetailResponse modifyPost(AppUser user, Long id, String title,
-                                         String content, LocalDateTime meetingTime,
-                                         String placeId, String placeName, String address,
-                                         Double latitude, Double longitude,
-                                         Integer maxParticipants, String openChatUrl, Integer deadLine) {
+        String content, LocalDateTime meetingTime,
+        String placeId, String placeName, String address,
+        Double latitude, Double longitude,
+        Integer maxParticipants, String openChatUrl, Integer deadLine) {
 
         Post post = findById(id);
 
@@ -243,10 +242,10 @@ public class PostService {
         }
 
         post.modify(
-                title, content, meetingTime,
-                placeId, placeName, address,
-                latitude, longitude,
-                maxParticipants, openChatUrl, deadLine
+            title, content, meetingTime,
+            placeId, placeName, address,
+            latitude, longitude,
+            maxParticipants, openChatUrl, deadLine
         );
 
         return post.toDetailResponse();
@@ -258,19 +257,19 @@ public class PostService {
     @Transactional
     public PostDetailResponse modifyPost(AppUser user, Long id, CreatePostRequest request) {
         return modifyPost(
-                user,
-                id,
-                request.getTitle(),
-                request.getContent(),
-                request.getMeetingTime(),
-                request.getPlaceId(),
-                request.getPlaceName(),
-                request.getAddress(),
-                request.getLatitude(),
-                request.getLongitude(),
-                request.getMaxParticipants(),
-                request.getOpenChatUrl(),
-                30 // 기본값으로 30분 전 설정
+            user,
+            id,
+            request.getTitle(),
+            request.getContent(),
+            request.getMeetingTime(),
+            request.getPlaceId(),
+            request.getPlaceName(),
+            request.getAddress(),
+            request.getLatitude(),
+            request.getLongitude(),
+            request.getMaxParticipants(),
+            request.getOpenChatUrl(),
+            30 // 기본값으로 30분 전 설정
         );
     }
 
@@ -290,18 +289,28 @@ public class PostService {
         postRepository.delete(post);
     }
 
-    public PostDetailResponse getPostDetails(Long postId) {
-        return findById(postId).toDetailResponse();
+    public PostDetailResponse getPostDetails(Long postId, Long userId) {
+        Post post = findById(postId);
+        PostDetailResponse postDetailResponse = post.toDetailResponse();
+
+        boolean isWriter = post.isWriterId(userId);
+        boolean isParticipant = post.getParticipations().stream()
+            .anyMatch(p -> p.getUser().getId().equals(userId));
+
+        postDetailResponse.setIIn(isWriter || isParticipant);
+
+        return postDetailResponse;
     }
 
     // 현재 모집 중인 모임 조회
     @Transactional
-    public Page<PostInfoResponse> getListOfPostInfo(Pageable pageable, String position, String keyword, AppUser user) throws JsonProcessingException {
+    public Page<PostInfoResponse> getListOfPostInfo(Pageable pageable, String position, String keyword, AppUser user)
+        throws JsonProcessingException {
         return findPostsByDistrictAndNeighborhood(pageable, position, keyword, user);
     }
 
     public Page<PostInfoResponse> getListOfCompletePostInfo(Pageable pageable, boolean recruitmentCompleted,
-                                                            boolean completed) {
+        boolean completed) {
         // 모집만 완료한 모임 또는 모임을 완료한 모임 조회
         Page<Post> posts = postRepository.findPostByCondition(pageable, recruitmentCompleted, completed);
         List<PostInfoResponse> postList = posts.stream().map(post -> {
@@ -332,11 +341,11 @@ public class PostService {
      * 주변 모임 검색 메서드
      */
     public Page<PostInfoResponse> findNearbyPosts(Double latitude, Double longitude, Double radiusKm, Pageable
-            pageable) {
+        pageable) {
         Page<Post> posts = postRepository.findNearbyPosts(latitude, longitude, radiusKm, pageable);
         List<PostInfoResponse> content = posts.getContent().stream()
-                .map(PostInfoResponse::new)
-                .collect(Collectors.toList());
+            .map(PostInfoResponse::new)
+            .collect(Collectors.toList());
 
         return new PageImpl<>(content, pageable, posts.getTotalElements());
     }
@@ -349,7 +358,7 @@ public class PostService {
      */
     @Transactional
     public Page<PostInfoResponse> findPostsByDistrictAndNeighborhood(Pageable pageable, String position, String
-            keyword, AppUser user) throws JsonProcessingException {
+        keyword, AppUser user) throws JsonProcessingException {
         // 검색하고 싶은 지역 찾기
         String[] regionFields = position.split(" ");
         Optional<Region> opRegion = regionService.findByDistrictAndNeighborhood(regionFields[0], regionFields[1]);
