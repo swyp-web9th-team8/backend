2b480b0dcc1f9e18b3cd7bf359860740b9766ef1
sspark <sauce0127@gmail.com>
2025-05-17 10:12:13 +0900

    fix : 공지사항 응답 객체 수정시간 추가

--- src/main/java/com/swyp/plogging/backend/announce/controller/dto/AnnounceResponse.java
+++ src/main/java/com/swyp/plogging/backend/announce/controller/dto/AnnounceResponse.java
@@ -1,6 +1,8 @@
 package com.swyp.plogging.backend.announce.controller.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.swyp.plogging.backend.announce.domain.Announce;
+import java.time.LocalDateTime;
 import lombok.Data;
 
 @Data
@@ -11,14 +13,19 @@ public class AnnounceResponse {
     private String content;
     private boolean active;
 
-    public AnnounceResponse(Long id, String title, String content, boolean active) {
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime lastModifiedDt;
+
+    public AnnounceResponse(Long id, String title, String content, boolean active, LocalDateTime lastModifiedDt) {
         this.id = id;
         this.title = title;
         this.content = content;
         this.active = active;
+        this.lastModifiedDt = lastModifiedDt;
     }
 
     public static AnnounceResponse from(Announce announce) {
-        return new AnnounceResponse(announce.getId(), announce.getTitle(), announce.getContent(), announce.isActive());
+        return new AnnounceResponse(announce.getId(), announce.getTitle(), announce.getContent(), announce.isActive(),
+            announce.getLastModifiedDt());
     }
 }
