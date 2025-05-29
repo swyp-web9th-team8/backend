e6ebfd417b4577cc3062ca51c7c5a36c50931164
geniushee <129508219+geniushee@users.noreply.github.com>
2025-05-29 18:11:04 +0900

    Hotfix/test#2 (#50)
    * 운영환경에서 안되는 부분 찾고 수정

--- src/main/java/com/swyp/plogging/backend/config/FCMConfig.java
+++ src/main/java/com/swyp/plogging/backend/config/FCMConfig.java
@@ -5,15 +5,18 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import jakarta.annotation.PostConstruct;
-import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.core.io.ClassPathResource;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 
 @Configuration
 public class FCMConfig {
+    @Autowired
+    private static Environment env;
     @PostConstruct
     public void init() throws IOException {
         FirebaseOptions options = new FirebaseOptions.Builder()
@@ -23,9 +26,14 @@ public class FCMConfig {
         FirebaseApp.initializeApp(options);
     }
 
-    @Bean
-    public static FileInputStream getCredentailFileInputStream() throws FileNotFoundException {
+    public static FileInputStream getCredentailFileInputStream() throws IOException {
+        String[] activeProfiles =  env.getActiveProfiles();
         // service-account.json 필요
+        for(String profile : activeProfiles){
+            if("prod".equals(profile)){
+                return (FileInputStream) new ClassPathResource("service_account.json").getInputStream();
+            }
+        }
         return new FileInputStream("src/main/resources/service_account.json");
     }
 }
