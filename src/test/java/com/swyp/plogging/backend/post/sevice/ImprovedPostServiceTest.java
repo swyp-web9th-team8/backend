17f0a1635bc0d04367f9c2cc4651e2d003ea8fdb
geniushee <h1116jeon@gmail.com>
2025-08-18 18:45:14 +0900

    test 초기 세팅 완료 - 테스트 시작을 위한 클래스 작성 - 지역 sql 삽입 - 프로파일 및 postgres 테스트컨테이너 설정 - 테스트에서 사용할 팩토리 작성

--- src/test/java/com/swyp/plogging/backend/post/sevice/ImprovedPostServiceTest.java
+++ src/test/java/com/swyp/plogging/backend/post/sevice/ImprovedPostServiceTest.java
@@ -1,19 +1,50 @@
 package com.swyp.plogging.backend.post.sevice;
 
+import com.swyp.plogging.backend.common.TestFactory;
+import com.swyp.plogging.backend.post.participation.service.ParticipationService;
+import com.swyp.plogging.backend.post.post.domain.Post;
+import com.swyp.plogging.backend.post.post.sevice.PostService;
+import com.swyp.plogging.backend.region.service.RegionService;
+import com.swyp.plogging.backend.user.user.domain.AppUser;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
 
 @Slf4j
 @SpringBootTest
 @ActiveProfiles("test")
+@Sql(scripts = "/sql/insert_data.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_CLASS)
 public class ImprovedPostServiceTest {
 
-    @Test
-    @DisplayName("contextLoad")
-    public void contextLoad(){
+    @Autowired
+    private ParticipationService participationService;
+    @Autowired
+    private PostService postService;
+    @Autowired
+    private RegionService regionService;
 
+    private static AppUser user;
+    private static Post post;
+
+
+
+    @BeforeEach
+    public void setUp(){
+        user = TestFactory.newUser("사용자1", "user@gmail.com");
+        post = TestFactory.newPostWithUser(user);
+        System.out.println(user);
+        System.out.println(post);
     }
+
+    @Test
+    @DisplayName("비관적락 테스트 - 1회")
+    public void test_pessimisticLockWithOneTime(){
+        log.info(user.toString() + post.toString());
+    }
+
 }
